d22d7a47cd5344177123d93b302f52840fc1aa81
Rui Vilela <tek@qwertytek.com>
2026-06-25 13:04:12 +0100

    fix deprecated eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,15 @@
 import eslint from '@eslint/js';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import prettier from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  {
-    ignores: [
-      '**/dist/**',
-      '**/coverage/**',
-      '**/node_modules/**',
-      '**/.aws-sam/**',
-    ],
-  },
+export default defineConfig(
+  globalIgnores([
+    '**/dist/**',
+    '**/coverage/**',
+    '**/node_modules/**',
+    '**/.aws-sam/**',
+  ]),
   eslint.configs.recommended,
   tseslint.configs.recommended,
   prettier,
